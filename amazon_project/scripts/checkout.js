829c1f86f95113307691babe6198a86e8c46f4e3
MateuszDlu <mateuszdluzniewski02@gmail.com>
2025-05-24 14:35:48 +0200

    products from API

--- amazon_project/scripts/checkout.js
+++ amazon_project/scripts/checkout.js
@@ -1,7 +1,10 @@
 import { renderOrderSummery } from "./checkout/orderSummary.js";
 import { renderPaymentSummery } from "./checkout/paymentSummery.js";
+import { loadProducts } from "../data/products.js";
 // import '../data/cart-class.js'
-import '../data/backend-practice.js'
+//import '../data/backend-practice.js'
 
-renderOrderSummery();
-renderPaymentSummery();
+loadProducts(() => {
+    renderOrderSummery();
+    renderPaymentSummery();
+})
