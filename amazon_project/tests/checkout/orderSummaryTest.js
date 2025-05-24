fa4c3d92969ac21464cab92d62661e434d8e98d3
MateuszDlu <mateuszdluzniewski02@gmail.com>
2025-05-24 14:42:53 +0200

    loadProducts() in beforeAll in tests

--- amazon_project/tests/checkout/orderSummaryTest.js
+++ amazon_project/tests/checkout/orderSummaryTest.js
@@ -1,9 +1,14 @@
 import { renderOrderSummery } from "../../scripts/checkout/orderSummary.js";
 import { loadFromStorage, updateDeliveryOption, cart } from "../../data/cart.js";
+import { loadProducts } from "../../data/products.js";
 
 describe('test suite: renderOrderSummary', () => {
     const productId1 = 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6'
     const productId2 = '15b6fc6f-327a-4ec4-896f-486349e85a3d'
+
+    beforeAll((done) => { // done is a Jasmine functions that makes the code synchronus until done() called
+        loadProducts(done)
+    })
 
     beforeEach(() => {
         document.querySelector('.js-test-container').innerHTML = `
